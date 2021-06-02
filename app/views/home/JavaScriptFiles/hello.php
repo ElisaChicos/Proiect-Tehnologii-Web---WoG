f15b57d2611ddfb034809c161d2e35ddaa54f93b
ElisaChicos <veronica.chicos@info.uaic.ro>
2021-06-02 21:11:03 +0300

    updates

---
+++ app/views/home/JavaScriptFiles/hello.php
@@ -0,0 +1,53 @@
+<h2> Set 2 </h2> 
+<br>
+<br>
+<br>
+<br><br>
+<br><br>
+<br><br>
+<br>
+        <div id="app"></div>
+        <button class="button2" onclick="startTimer()">START EXERCISE</button>
+        <button class="button" id="getTxt">NEXT EXERCISE</button>
+
+<img class="exercise" src="
+<?php
+session_start();
+$host="localhost";
+$user="root";
+$password="";
+$db="images";
+$aVar=mysqli_connect($host,$user,$password);
+mysqli_select_db($aVar,'user_exemplu');
+$sql = "SELECT * FROM user_exemplu.images WHERE id='2';";
+$result = mysqli_query($aVar,$sql) or die( mysqli_error($aVar));
+while($row = mysqli_fetch_array($result))
+  {
+    echo $row["path"];
+  }
+
+?>
+">
+
+<div class="exercise_info">
+<?php
+$sql = "SELECT * FROM user_exemplu.images WHERE id='2';";
+$result = mysqli_query($aVar,$sql) or die( mysqli_error($aVar));
+while($row = mysqli_fetch_array($result))
+  {
+    
+    echo "<div class=\"title\"> Proper breathing</div> <br>";
+    echo "<div class=\"enum\">".$row["breathing_info"] ."</div> <br>";
+    echo "<br><br><br><br><br>";
+    echo "<div class=\"title\"> Steps</div> <br>";
+    echo "<div class=\"enum\">".$row["step1"] ." <br>";
+    echo $row["step2"] ."<br>";
+    echo $row["step3"] ."<br>";
+    echo $row["step4"] ."</div><br>";
+    echo "<br><br><br><br><br>";
+    echo "<div class=\"title\"> Sets & Reps</div> <br>";
+    echo "<div class=\"enum\">".$row["reps"] ."</div><br>";
+  }
+?>
+
+</div>
