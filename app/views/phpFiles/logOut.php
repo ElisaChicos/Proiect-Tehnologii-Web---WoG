249a2423bfbd95a76c2300d33efd022a97872da2
Haidau Alina <alina.haidau@students.info.uaic.ro>
2021-06-22 06:33:04 +0300

    main

--- app/views/phpFiles/logOut.php
+++ app/views/phpFiles/logOut.php
@@ -5,17 +5,9 @@ if (isset($_POST['logout'])) {
 
     if (!isset($_SESSION)) {
         echo 'not destroyed';
-    }else{
-    $_SESSION = array();
-    if (ini_get("session.use_cookies")) {
-        $params = session_get_cookie_params();
-        setcookie(session_name(), '', time() - 42000,
-            $params["path"], $params["domain"],
-            $params["secure"], $params["httponly"]
-        );
     }
     session_destroy();
     header('Location: /app/views/home/Login.php'); 
-    }  
+    
 }
 ?>
